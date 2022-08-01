adb0e724a71fc41397e4fc58a94ace1ce047e26f
radabp <58158602+mbadarp@users.noreply.github.com>
2022-08-01 20:01:58 +0700

    last update maybe

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+/*--====== Resize Navbar on Scroll ======--*/
+var navbar = document.querySelector(".navbar");
+//when the scroll is higher than 20 viewport height, add the sticky class to the tag with a class navbar
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+
+}
